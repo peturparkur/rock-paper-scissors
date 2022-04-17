f1ae5c208ea8c71a91ec9d6f7a14ca4957d7faf7
Tamanna <tamannadebnathf@gmail.com>
2022-04-17 13:51:38 +0100

    documentation

--- src/agent.py
+++ src/agent.py
@@ -1,4 +1,18 @@
 
 class Agent:
+    """
+    Base Agent for interface with Rock-Paper-Scissors environment
+    """
     def turn(self, state : dict):
+        """
+        Called at every turn of rock-paper-scissors
+
+        Params
+        ------
+        @state: The current environment status as JSON / Dictionary
+
+        Returns
+        ------
+        @int: number between [0, 3) which represents (Rock, Paper, Scissors)
+        """
         pass
